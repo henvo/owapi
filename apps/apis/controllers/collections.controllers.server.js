cc5a945a853ba71e2ab4354b1db8f30f3e6de390
Henning Vogt <git@henvo.de>
2015-09-09 02:05:58 +0200

    Added CRD for api collections

--- apps/apis/controllers/collections.controllers.server.js
+++ apps/apis/controllers/collections.controllers.server.js
@@ -1,19 +1,43 @@
+var mongoose = require('mongoose')
+
 module.exports = {
 
   create: function(req, res) {
-    res.send('Create a new collection')
+    if(!req.api.resources) req.api.resources = {}
+    req.api.resources[req.body.name] = req.body.schema
+    req.api.markModified('resources')
+    req.api.save(function (err) {
+      if(err) {
+        res.send(err)
+      }
+      else {
+        res.send(req.api)
+      }
+    })
   },
   read: function(req, res) {
-    res.send('Get collection information')
+    res.send(req.api.resources[req.params.resourceName])
   },
   update: function(req, res) {
-    res.send('Update Collection')
+    res.send('Update Collection is not possible')
   },
   list: function(req, res) {
-    res.send('List all Collections')
+    res.send(req.api.resources)
   },
   remove: function(req, res) {
-    res.send('Delete Collection')
+    delete req.api.resources[req.params.resourceName]
+    req.api.markModified('resources')
+    req.api.save(function (err) {
+      if(err) {
+        res.send(err)
+      }
+      else {
+        mongoose.connection.collections[req.modelName].drop( function(err) {
+          if(err) res.send(err)
+          else res.send(req.api)
+        })
+      }
+    })
   }
 
 }
